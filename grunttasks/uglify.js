a8445cc44a1f4025de0427ad39414342e7fa9fa2
Shane Tomlinson <stomlinson@mozilla.com>
2013-12-27 16:46:34 +0000

    Start to set up for grunt.

---
+++ grunttasks/uglify.js
@@ -0,0 +1,22 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+const path = require('path');
+
+module.exports = function (grunt) {
+  'use strict';
+
+  grunt.config('uglify', {
+    options: {
+      banner: grunt.file.read(path.join(__dirname, '..', 'LICENSE'))
+    },
+    dist: {
+      files: {
+        'client/dist/include.min.js': [ ],
+        'client/dist/rum-diary.min.js': [ ]
+      }
+    }
+  });
+};
+
