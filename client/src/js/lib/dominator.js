64297d83eb402f1657bbd7ff0456e89c96060cd7
Shane Tomlinson <stomlinson@mozilla.com>
2014-01-03 18:29:31 +0000

    use the newest dominator

--- client/src/js/lib/dominator.js
+++ client/src/js/lib/dominator.js
@@ -1,163 +1,143 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 window.DOMinator = (function() {
   'use strict';
 
-  function flatten(array) {
-    return [].reduce.call(array, function(prevValue, currValue) {
-      if ('length' in currValue)
-        return prevValue.concat([].slice.call(currValue, 0));
+  /*global document*/
 
-      return prevValue.push(currValue);
-    }, []);
-  }
-
-  function isString(itemToCheck) {
-    return '[object String]' === Object.prototype.toString.apply(itemToCheck);
-  }
-
-  function isArray(itemToCheck) {
-    return '[object Array]' === Object.prototype.toString.apply(itemToCheck);
-  }
-
-  function getElements(selector) {
-      if (! selector) return [];
-
-      if (isString(selector)) {
-        selector = selector.trim();
-        if (selector[0] === "<") {
-          // HTML was specified! Create the elements
-          var element = document.createElement('div');
-          element.innerHTML = selector;
-          return getElements(element.childNodes);
-        }
-
-        // Just a normal selector
-        return getElements(document.querySelectorAll(selector));
-      }
-
-      // already an array, just return the array.
-      if (isArray(selector))
-          return selector;
-
-      // an array-like object, conver to an array.
-      if ('length' in selector)
-          return [].slice.call(selector, 0);
-
-      return [ selector ];
-  }
-
-  function isValBased(target) {
-      var elName = target.nodeName.toLowerCase();
-      return elName === 'input' || elName === 'textarea';
-  }
-
-  function createChain(selector) {
-    var chained = Object.create(Chained);
-    chained.init(selector);
-    return chained;
-  }
-
-  var Chained = {
-    init: function(selector) {
+  var DOMinator = {
+    /**
+     * Fill the DOMinator object with the selector
+     */
+    fill: function(selector) {
       var els = getElements(selector);
+
       els.forEach(function(item, index) {
         this[index] = item;
       }, this);
       this.length = els.length;
+
       return this;
     },
 
+    /**
+     * Get the nth object in the set
+     */
     nth: function(index) {
       return this[index];
     },
 
+    /**
+     * Get all the children of all the elements in the set
+     */
     children: function() {
-      return createChain(flatten(this.map(function(element) {
+      return toDOMinator(flatten(this.toArray().map(function(element) {
         return element.childNodes;
       })));
     },
 
+    /**
+     * Get the nth child of all the children of all the elements in the set
+     */
     nthChild: function(index) {
       return this.children()[index];
     },
 
+    /**
+     * Find descendent elements
+     */
     find: function(selector) {
-      return createChain(flatten(this.map(function(element) {
+      return toDOMinator(flatten(this.toArray().map(function(element) {
         return element.querySelectorAll(selector);
       })));
     },
 
+    /**
+     * Find descendent elements and include the root if it matches the selector
+     */
     findIncludeRoot: function(selector) {
       var els = [].slice.call(this.find(selector), 0);
 
       this.forEach(function(element) {
-        if (createChain(element).is(selector)) {
+        if (toDOMinator(element).is(selector)) {
           // TODO, this is going to put elements on the front in reverse order.
           els.unshift(element);
         }
       });
 
-      return createChain(els);
+      return toDOMinator(els);
     },
 
+    /**
+     * Check if the first element of the set matches the type
+     */
     is: function(type) {
-      var haystack = createChain(type);
+      if (isEmpty(this)) return false;
 
-      var needle = this[0];
-      if (! needle) return false;
-
-      return haystack.indexOf(needle) > -1;
+      var haystack = toDOMinator(type).toArray();
+      return haystack.indexOf(this[0]) > -1;
     },
 
+    /**
+     * Find the closest ancestor of the first element of the set
+     * that matches the selector
+     */
     closest: function(selector) {
       var target = this[0];
 
       while (target) {
-        var chained = createChain(target);
+        var chained = toDOMinator(target);
         if (chained.is(selector)) return chained;
         target = target.parentNode;
       }
     },
 
+    /**
+     * Remove the set of elements from the DOM
+     */
     remove: function() {
-      this.forEach(function(element) {
+      return this.forEach(function(element) {
         element.parentNode.removeChild(element);
       });
-
-      return this;
     },
 
+    /**
+     * Add a DOM event handler to the set of elements
+     */
     bindEvent: function(eventName, callback, bubble) {
-      this.forEach(function(element) {
+      return this.forEach(function(element) {
         element.addEventListener(eventName, callback, bubble);
       });
-
-      return this;
     },
 
+    /**
+     * Remove a DOM event handler from the set of elements
+     */
     unbindEvent: function(eventName, callback, bubble) {
-      this.forEach(function(element) {
+      return this.forEach(function(element) {
         element.removeEventListener(eventName, callback, bubble);
       });
-
-      return this;
     },
 
+    /**
+     * Fire a synthetic event on the set of elements
+     */
     fireEvent: function(type) {
-      this.forEach(function(element) {
-          var event = document.createEvent('CustomEvent');
-          event.initCustomEvent(type, true, true, undefined);
-          element.dispatchEvent(event);
+      return this.forEach(function(element) {
+        var event = document.createEvent('CustomEvent');
+        event.initCustomEvent(type, true, true, undefined);
+        element.dispatchEvent(event);
       });
-
-      return this;
     },
 
+    /**
+     * Get/Set the innerHTML or value of an element.
+     */
     inner: function(value) {
       if (arguments.length === 0) {
-        var target = this.nth(0);
+        var target = this[0];
         if (! target) return;
 
         if(isValBased(target)) {
@@ -167,7 +147,7 @@ window.DOMinator = (function() {
         return target.innerHTML;
       }
 
-      this.forEach(function(element) {
+      return this.forEach(function(element) {
         if(isValBased(element)) {
           element.value = value;
         }
@@ -175,161 +155,237 @@ window.DOMinator = (function() {
           element.innerHTML = value;
         }
       });
-
-      return this;
     },
 
+    /**
+     * Remove all children from the set of elements
+     */
     empty: function() {
-      this.forEach(function(element) {
-        element.innerHTML = '';
+      return this.forEach(function(element) {
+        toDOMinator(element).inner('');
       });
     },
 
+    /**
+     * Get/Set an attribute on the set of elements
+     */
     attr: function(attrName, value) {
       if (arguments.length === 1) {
-        var element = this.nth(0);
+        var element = this[0];
         if (! element) return;
         return element.getAttribute(attrName);
       }
 
-      this.forEach(function(element) {
+      return this.forEach(function(element) {
         element.setAttribute(attrName, value);
       });
-
-      return this;
     },
 
+    /**
+     * Check if the first element in the set has an attribute
+     */
     hasAttr: function(attrName) {
-      var element = this.nth(0);
-      if (! element) return false;
-      return !! element.hasAttribute(attrName);
+      if (isEmpty(this)) return false;
+      return !! this[0].hasAttribute(attrName);
     },
 
+    /**
+     * Remove the attribute from all elements in the set
+     */
     removeAttr: function(attrName) {
-      this.forEach(function(element) {
+      return this.forEach(function(element) {
         element.removeAttribute(attrName);
       });
-
-      return this;
     },
 
+    /**
+     * Add a class to all elements in the set
+     */
     addClass: function(className) {
-      this.forEach(function(element) {
+      return this.forEach(function(element) {
         element.classList.add(className);
       });
-
-      return this;
     },
 
+    /**
+     * Remove a class from all elements in the set
+     */
     removeClass: function(className) {
-      this.forEach(function(element) {
+      return this.forEach(function(element) {
         element.classList.remove(className);
       });
+    },
 
+    /**
+     * Check if the first element in the set has a class name
+     */
+    hasClass: function(className) {
+      if (isEmpty(this)) return false;
+      return this[0].classList.contains(className);
+    },
+
+    /**
+     * Iterate over the elements
+     */
+    forEach: function(callback, context) {
+      [].forEach.call(this, callback, context);
       return this;
     },
 
-    hasClass: function(className) {
-      var element = this.nth(0);
-      if (! element) return false;
-      return element.classList.contains(className);
-    },
-
+    /**
+     * Append an element to all elements in the set
+     */
     append: function(elementToAppend) {
-      this.forEach(function(parent) {
-        createChain(elementToAppend).forEach(function(element) {
+      return this.forEach(function(parent) {
+        toDOMinator(elementToAppend).forEach(function(element) {
           parent.appendChild(element);
         });
       });
-
-      return this;
     },
 
+    /**
+     * Append the current set of elements to another element
+     */
     appendTo: function(elementToAppendTo) {
-      var parentNode = createChain(elementToAppendTo).nth(0);
+      var parentNode = toDOMinator(elementToAppendTo)[0];
       if (! parentNode) return;
 
-      this.forEach(function(element) {
+      return this.forEach(function(element) {
         parentNode.appendChild(element);
       });
-
-      return this;
     },
 
+    /**
+     * Insert the current set of elements after another element
+     */
     insertAfter: function(elementToInsertAfter) {
-      var insertAfter = createChain(elementToInsertAfter).nth(0);
+      var insertAfter = toDOMinator(elementToInsertAfter)[0];
       if (! insertAfter) return;
 
       var insertBefore = insertAfter.nextChild || null;
       var parentNode = insertAfter.parentNode;
 
-      this.forEach(function(element) {
+      return this.forEach(function(element) {
         parentNode.insertBefore(element, insertBefore);
       });
-
-      return this;
     },
 
+    /**
+     * Insert the current set of elements before another element
+     */
     insertBefore: function(elementToInsertBefore) {
-      var insertBefore = createChain(elementToInsertBefore).nth(0);
+      var insertBefore = toDOMinator(elementToInsertBefore)[0];
       if (! insertBefore) return;
 
       var parentNode = insertBefore.parentNode;
 
-      this.forEach(function(element) {
+      return this.forEach(function(element) {
         parentNode.insertBefore(element, insertBefore);
       });
-
-      return this;
     },
 
+    /**
+     * Insert the current set of elements as the Nth child of another element
+     */
     insertAsNthChild: function(parent, index) {
-      var nthChild = createChain(parent).nthChild(index);
+      var nthChild = toDOMinator(parent).nthChild(index);
       if (! nthChild) return;
 
-      this.forEach(function(element) {
+      return this.forEach(function(element) {
         nthChild.parentElement.insertBefore(element, nthChild);
       });
-
-      return this;
     },
 
+    /**
+     * Focus the first element in the set
+     */
     focus: function() {
-      var target = this.nth(0);
-      if (! target) return;
-      target.focus();
+      if (isEmpty(this)) return;
 
+      this[0].focus();
       return this;
     },
 
+    /**
+     * Show all elements in the set by setting 'display: block'
+     */
     show: function() {
       return this.style('display', 'block');
     },
 
+    /**
+     * Hide all elements in the set by setting 'display: none'
+     */
     hide: function() {
       return this.style('display', 'none');
     },
 
+    /**
+     * Set a style on all elements in the set
+     */
     style: function(name, value) {
-      this.forEach(function(element) {
+      return this.forEach(function(element) {
         element.style[name] = value;
       });
+    },
 
-      return this;
+    /**
+     * Convert the set to an Array
+     */
+    toArray: function() {
+      return [].slice.call(this, 0);
     }
   };
 
-  [
-    'map',
-    'indexOf',
-    'forEach'
-  ].forEach(function(key) {
-    Chained[key] = function() {
-      return [][key].apply(this, arguments);
-    };
-  });
+  function flatten(array) {
+    return [].reduce.call(array, function(prevValue, currValue) {
+      return prevValue.concat([].slice.call(currValue, 0));
+    }, []);
+  }
 
-  return createChain;
+  function isString(itemToCheck) {
+    return '[object String]' === Object.prototype.toString.apply(itemToCheck);
+  }
+
+  function getElements(selector) {
+    if ( ! selector) return [];
+
+    if (isString(selector)) {
+      selector = selector.trim();
+      if (selector[0] === "<") {
+        // HTML was specified! Create the elements
+        var element = document.createElement('div');
+        element.innerHTML = selector;
+        return getElements(element.childNodes);
+      }
+
+      // Just a normal selector
+      return getElements(document.querySelectorAll(selector));
+    }
+
+    // an array or array-like object, make a copy or convert to an array.
+    if ('length' in selector)
+        return [].slice.call(selector, 0);
+
+    return [ selector ];
+  }
+
+  function isValBased(target) {
+    var elName = target.nodeName.toLowerCase();
+    return elName === 'input' || elName === 'textarea';
+  }
+
+  function isEmpty(dominator) {
+    return !dominator.length;
+  }
+
+  function toDOMinator(selector) {
+    var dom = Object.create(DOMinator);
+    dom.fill(selector);
+    return dom;
+  }
+
+  return toDOMinator;
 
 }());
 
