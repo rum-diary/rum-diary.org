0bdf6d80e8386fef686c49ca2ba6d743ab7a665b
Shane Tomlinson <stomlinson@mozilla.com>
2014-03-05 08:27:54 +0000

    refactor(server): Use .getStream instead of .get in GET-site-hostname-demographics.

--- server/routes/GET-site-hostname-demographics.js
+++ server/routes/GET-site-hostname-demographics.js
@@ -17,31 +17,40 @@ exports.handler = function(req, res) {
   var start = moment(query.createdAt.$gte);
   var end = moment(query.createdAt.$lte);
 
-  db.pageView.get(query)
-    .then(function(hits) {
-      return reduce.mapReduce(hits, [
-        'browsers',
-        'os',
-        'os:form'
-      ], {
-        start: start,
-        end: end
+  var reduceStream = new reduce.StreamReduce({
+    which: [
+      'browsers',
+      'os',
+      'os:form'
+    ],
+    start: start,
+    end: end
+  });
+
+  db.pageView.getStream(query)
+    .then(function (stream) {
+      stream.on('data', reduceStream.write.bind(reduceStream));
+
+      stream.on('close', complete);
+
+      stream.on('err', function (err) {
+        res.send(500);
+        logger.error(String(err));
       });
-    })
-    .then(function(data) {
-      logger.info('os data', JSON.stringify(data.os));
-      res.render('GET-site-hostname-demographics.html', {
-        hostname: req.params.hostname,
-        resources: clientResources('rum-diary.min.js'),
-        startDate: start.format('MMM DD'),
-        endDate: end.format('MMM DD'),
-        browsers: data.browsers,
-        os: data.os,
-        os_form: data['os:form']
-      });
-    }).catch(function(err) {
-      logger.error(String(err));
-      res.send(500);
     });
+
+  function complete() {
+    var result = reduceStream.result();
+    logger.info('os result', JSON.stringify(result.os));
+    res.render('GET-site-hostname-demographics.html', {
+      hostname: req.params.hostname,
+      resources: clientResources('rum-diary.min.js'),
+      startDate: start.format('MMM DD'),
+      endDate: end.format('MMM DD'),
+      browsers: result.browsers,
+      os: result.os,
+      os_form: result['os:form']
+    });
+  }
 };
 
