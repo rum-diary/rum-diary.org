888c72b70c857c2dcd870c02878e0815cf3f04ba
Shane Tomlinson <stomlinson@mozilla.com>
2014-03-24 21:17:01 +0000

    refactor(test): Split up db_test.js into individual model tests.

---
+++ test/models/user_test.js
@@ -0,0 +1,85 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/*global describe, beforeEach, afterEach, it*/
+
+const mocha = require('mocha');
+const assert = require('chai').assert;
+
+const db = require('../../server/lib/db');
+const user = db.user;
+
+const testExtras = require('../lib/test-extras');
+const cPass = testExtras.cPass;
+const fail = testExtras.fail;
+
+describe('database', function () {
+  beforeEach(function (done) {
+    db.clear(done);
+  });
+
+  afterEach(function (done) {
+    db.clear(done);
+  });
+
+  describe('user', function () {
+    describe('create', function () {
+      it('should create an item', function (done) {
+        user.create({
+          name: 'Site Administrator',
+          email: 'testuser@testuser.com'
+        }).then(function (user) {
+          assert.equal(user.name, 'Site Administrator');
+          assert.equal(user.email, 'testuser@testuser.com');
+
+          done();
+        }, fail);
+      });
+    });
+
+    describe('get', function () {
+      it('should get one or more saved users', function (done) {
+        user.create({
+          name: 'Site Administrator',
+          email: 'testuser@testuser.com'
+        }).then(function () {
+          return user.create({
+            name: 'Another Administrator',
+            email: 'testuser2@testuser.com'
+          });
+        }).then(function () {
+          return user.get({
+            email: 'testuser2@testuser.com'
+          });
+        }).then(function (users) {
+          assert.equal(users[0].name, 'Another Administrator');
+          assert.equal(users[0].email, 'testuser2@testuser.com');
+          done();
+        }, fail);
+      });
+    });
+
+    describe('getOne', function () {
+      it('should get a saved user', function (done) {
+        user.create({
+          name: 'Site Administrator',
+          email: 'testuser@testuser.com'
+        }).then(function () {
+          return user.create({
+            name: 'Another Administrator',
+            email: 'testuser2@testuser.com'
+          });
+        }).then(function () {
+          return user.getOne({
+            email: 'testuser@testuser.com'
+          });
+        }).then(function (user) {
+          assert.equal(user.name, 'Site Administrator');
+          assert.equal(user.email, 'testuser@testuser.com');
+          done();
+        }, fail);
+      });
+    });
+  });
+});
