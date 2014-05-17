dbbaeb1cbcfb289606600b47d329186fba7c11a0
Shane Tomlinson <stomlinson@mozilla.com>
2014-05-17 16:22:25 +0100

    fix(build): Do not copy .scss files to dist.
    issue #27

--- grunttasks/copy.js
+++ grunttasks/copy.js
@@ -11,7 +11,7 @@ module.exports = function (grunt) {
         // most files
         expand: true,
         cwd: '<%= app.src %>',
-        src: [ 'css/*', 'i/*', 'js/**/*.js', 'js/**/*.css' ],
+        src: [ 'css/*.css', 'i/*', 'js/**/*.js', 'js/**/*.css' ],
         dest: '<%= app.dist %>'
       }, {
         // uncompressed include.js
