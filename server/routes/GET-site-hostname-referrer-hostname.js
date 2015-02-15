236cd81f10fee7eb370ca78dae7ac4e58b4374bb
Shane Tomlinson <stomlinson@mozilla.com>
2015-02-15 00:16:45 +0000

    extract the site-referrer stuff to rum-diary-calculator

--- server/routes/GET-site-hostname-referrer-hostname.js
+++ server/routes/GET-site-hostname-referrer-hostname.js
@@ -6,7 +6,7 @@ const Promises = require('bluebird');
 const logger = require('../lib/logger');
 const db = require('../lib/db');
 const siteCollection = db.site;
-const calculator = require('../lib/calculator');
+const calculator = require('rum-diary-calculator')({ db: db });
 const clientResources = require('../lib/client-resources');
 
 exports.path = '/site/:hostname/referrer/:referrer';
@@ -18,45 +18,24 @@ exports.locals = {
 exports.authorization = require('../lib/page-authorization').CAN_READ_HOST;
 
 exports.handler = function (req) {
-  var pageViewQuery = req.dbQuery;
-  pageViewQuery.hostname = req.params.hostname;
-  pageViewQuery.referrer_hostname = req.params.referrer;
+  var hostname = req.params.hostname;
+  var referrerHostname = req.params.referrer;
+  var startDate = req.start;
+  var endDate = req.end;
 
   return Promises.all([
     siteCollection.isAuthorizedToAdministrate(req.session.email, req.params.hostname),
-    calculator.calculate({
-      pageView: {
-        filter: pageViewQuery,
-        'referrers': {},
-      }
-    })
-  ]).then(function (allResults) {
-    var isAdmin = allResults[0];
-    var calculatorResults = allResults[1];
-
-    logger.info('%s: elapsed time: %s ms', req.url, calculatorResults.duration);
-
-    var referralsByPath = calculatorResults.pageView.referrers.by_hostname_to_path[req.params.referrer];
-    var referrals = referralsByPath ? sortReferrals(referralsByPath) : null;
+    calculator.siteReferrer(hostname, referrerHostname, startDate, endDate)
+  ]).spread(function (isAdmin, referrals) {
+    logger.info('%s: elapsed time: %s ms', req.url, referrals.duration);
 
     return {
       isAdmin: isAdmin,
       hostname: req.params.hostname,
       referrer: req.params.referrer,
-      referrals: referrals,
+      referrals: referrals.referrals,
       startDate: req.start,
       endDate: req.end
     };
   });
-
-  function sortReferrals(referralsByPath) {
-    return Object.keys(referralsByPath).map(function(path) {
-                      return {
-                        path: path,
-                        count: referralsByPath[path]
-                      };
-                    }).sort(function (a, b) {
-                      return b.count - a.count;
-                    });
-  }
 };
